ccf27bb99ee561ef7e32f628ebaec5af601a2162
naroqwe <49810638+naroqwe@users.noreply.github.com>
2023-10-16 21:38:47 +0200

    finished main logic

--- main.js
+++ main.js
@@ -1,34 +1,48 @@
 const containerDiv = document.querySelector(".container");
 const button = document.querySelector(".button");
 const sizeOfGrid = document.querySelector(".grid-size");
+const eraser = document.querySelector(".eraser");
+const pen = document.querySelector(".pen");
 
-function createDivs(size) {
+function createDivs() {
   let gridSize = sizeOfGrid.value;
 
+  containerDiv.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  containerDiv.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
   console.log(gridSize);
-  containerDiv.innerHTML = "";
-  // let newDiv = document.createElement("div");
-  // newDiv.classList.add("sketch-divs");
-  // containerDiv.appendChild(newDiv);
+
   for (let i = 0; i < gridSize * gridSize; i++) {
     let newDiv = document.createElement("div");
-    newDiv.classList.add("sketch-divs");
-    newDiv.textContent = i + 1;
+
+    newDiv.addEventListener("mousemove", function () {
+      if (pen.checked == true) {
+        newDiv.style.backgroundColor = "green";
+      } else if (eraser.checked == true) {
+        newDiv.style.backgroundColor = "grey";
+      }
+    });
     containerDiv.appendChild(newDiv);
   }
-
-  containerDiv.style.gridTemplateColumns = `repeat(${gridSize}, 50px)`;
 }
 
 function createOnLoad() {
+  containerDiv.style.gridTemplateColumns = "repeat(16, 1fr)";
+  containerDiv.style.gridTemplateRows = "repeat(16, 1fr)";
+
   for (let i = 0; i < 16 * 16; i++) {
     let newDiv = document.createElement("div");
-    newDiv.classList.add("sketch-divs");
-    newDiv.textContent = i + 1;
+
+    newDiv.addEventListener("mousemove", function () {
+      if (pen.checked == true) {
+        newDiv.style.backgroundColor = "green";
+      } else if (eraser.checked == true) {
+        newDiv.style.backgroundColor = "grey";
+      }
+    });
     containerDiv.appendChild(newDiv);
   }
-
-  containerDiv.style.gridTemplateColumns = `repeat(${16}, 50px)`;
 }
+
 window.addEventListener("load", createOnLoad);
 button.addEventListener("click", createDivs);
