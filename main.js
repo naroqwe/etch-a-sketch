5f163fbe50655573249a93eb2201a591f638bea9
naroqwe <49810638+naroqwe@users.noreply.github.com>
2023-10-17 21:50:15 +0200

    almost finished whole project

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 const containerDiv = document.querySelector(".container");
 const button = document.querySelector(".button");
 const sizeOfGrid = document.querySelector(".grid-size");
-const eraser = document.querySelector(".eraser");
-const pen = document.querySelector(".pen");
+const eraser = document.querySelector("#eraser");
+const pen = document.querySelector("#pen");
+const color = document.querySelector("#color-picker");
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
 
 function createDivs() {
   let gridSize = sizeOfGrid.value;
 
   containerDiv.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
   containerDiv.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
-
-  console.log(gridSize);
-
+  if (gridSize > 64) {
+    alert("MAXIMUM GRID SIZE IS 64");
+    createOnLoad();
+    return;
+  }
   for (let i = 0; i < gridSize * gridSize; i++) {
     let newDiv = document.createElement("div");
 
@@ -33,9 +40,16 @@ function createOnLoad() {
   for (let i = 0; i < 16 * 16; i++) {
     let newDiv = document.createElement("div");
 
-    newDiv.addEventListener("mousemove", function () {
+    // newDiv.addEventListener("mousedown", function () {
+    //   if (pen.checked == true) {
+    //     newDiv.style.backgroundColor = color.value;
+    //   } else if (eraser.checked == true) {
+    //     newDiv.style.backgroundColor = "grey";
+    //   }
+    // });
+    newDiv.addEventListener("pointerenter", (e) => {
       if (pen.checked == true) {
-        newDiv.style.backgroundColor = "green";
+        newDiv.style.backgroundColor = color.value;
       } else if (eraser.checked == true) {
         newDiv.style.backgroundColor = "grey";
       }
@@ -44,5 +58,5 @@ function createOnLoad() {
   }
 }
 
-window.addEventListener("load", createOnLoad);
+window.addEventListener("DOMContentLoaded", createOnLoad);
 button.addEventListener("click", createDivs);
